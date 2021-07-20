d39c409b38ee9a9403e8db64260e1bc1c948dc23
John Freitas <john.freitasau@gmail.com>
2021-07-21 07:49:47 +1000

    Validated user permissions via SSR

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,18 +1,20 @@
-import { useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
+import { Can } from "../components/Can";
+
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
-  console.log("USER:", user);
+  // console.log("USER:", user);
 
-  const userCanSeeMetrics = useCan({
-    permissions: ["metrics.list"],
-    roles: ["editor"],
-  });
+  // const userCanSeeMetrics = useCan({
+  //   permissions: ["metrics.list"],
+  //   roles: ["editor"],
+  // });
 
   useEffect(() => {
     api.get("/me").then((response) => response);
@@ -22,7 +24,9 @@ export default function Dashboard() {
     <>
       <h1>Dashboard: {user?.email}</h1>
 
-      {userCanSeeMetrics && <div>Metrics</div>}
+      <Can permissions={["metrics.list"]}>
+        <div>Metrics</div>
+      </Can>
     </>
   );
 }
